9f8f6541e3387afa81ad68df58ff5dd18fd9d099
Marius <mariusc0023@gmail.com>
2025-09-10 00:05:23 +0300

    Update settings.py

--- lvg_webapp/lvg_webapp/settings.py
+++ lvg_webapp/lvg_webapp/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+u%0p(@j+p%_srbd7v3d==ka$m2-4$t@y#vdk5te&crjl61r4'
+SECRET_KEY = 'django-insecure'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER  = 'mariusc0023@gmail.com'
-EMAIL_HOST_PASSWORD = 'udqdkllwjqjilwsp'
+EMAIL_HOST_USER  = ''
+EMAIL_HOST_PASSWORD = ''
